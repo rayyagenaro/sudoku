3b51567f100c4571a3642eb8917828845cd1c34b
afandiwirawan <147696851+afandiwirawan@users.noreply.github.com>
2023-12-05 09:17:48 +0700

    Initial commit

---
+++ src/sudoku/SudokuMain.java
@@ -0,0 +1,42 @@
+package sudoku;
+import java.awt.*;
+import javax.swing.*;
+/**
+ * The main Sudoku program
+ */
+public class SudokuMain extends JFrame {
+    private static final long serialVersionUID = 1L;  // to prevent serial warning
+
+    // private variables
+    GameBoardPanel board = new GameBoardPanel();
+    JButton btnNewGame = new JButton("New Game");
+
+    // Constructor
+    public SudokuMain() {
+        Container cp = getContentPane();
+        cp.setLayout(new BorderLayout());
+
+        cp.add(board, BorderLayout.CENTER);
+
+        // Add a button to the south to re-start the game via board.newGame()
+        // ......
+
+        // Initialize the game board to start the game
+        board.newGame();
+
+        pack();     // Pack the UI components, instead of using setSize()
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // to handle window-closing
+        setTitle("Sudoku");
+        setVisible(true);
+    }
+
+    /** The entry main() entry method */
+    public static void main(String[] args) {
+        // Run GUI codes in the Event-Dispatching thread for thread safety
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new SudokuMain();  // Let the constructor do the job
+            }
+        });
+    }
+}
